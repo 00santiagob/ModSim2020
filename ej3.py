255d6989526c94cd8b460063598bc5913f45fdfa
00santiagob <santiagobalog1998@gmail.com>
2020-06-12 23:04:39 -0300

    Parcial terminado, falto completar ej3.py

--- ej3.py
+++ ej3.py
@@ -10,26 +10,14 @@ def g(u):  # funcion a integrar en (0,1)
 
 def MonteCarlo(g, Nsim):
     # estimacion por el Monte Carlo con N simulaciones
-    # se modifico para que almacene los g(u)
     integral = 0
-    gu = []
-    for i in range(Nsim):
-        gu.append(g(random()))
-        integral += gu[i]
-    return integral/Nsim, gu
-
-
-def DesvioEstandar_X(N, Media, Xi):
-    S = 0
-    for i in range(N):
-        S += (Xi[i] - Media)**2
-    return np.sqrt(S/(N-1))
+    for _ in range(Nsim):
+        integral += g(random())
+    return integral/Nsim
 
 
 if __name__ == "__main__":  # Ejercicio
     for n in [1000, 10000, 100000]:
         print("\nCon {} simulaciones:".format(n))
-        MediaI, gu = MonteCarlo(g, n)
-        S = DesvioEstandar_X(n, MediaI, gu)
+        MediaI = MonteCarlo(g, n)
         print("Monte Carlo, I =", MediaI)
-        print("Desviacion Estandar S =", S)
